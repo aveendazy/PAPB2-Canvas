c82ca7c9b8c1c4d211e381cd2731b2a86c516120
aveendazy <zaveenda@mail.ugm.ac.id>
2022-04-28 22:59:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/canvasapp/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.canvasapp;
+
+import androidx.annotation.Keep;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+    private Canvas mCanvas;
+    private Paint mPaint = new Paint();
+    private Paint mPaintText = new Paint(Paint.UNDERLINE_TEXT_FLAG);
+    private Bitmap mBitmap;
+
+    private int mColorRect;
+    private int mColorBg;
+
+    private final static int OFFSET = 120;
+    private int mOffset = OFFSET;
+
+    private final static int MULTIPLIER= 100;
+
+    private Rect mRect;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.myimageview);
+
+        mColorRect = ResourcesCompat.getColor(getResources(), R.color.colorRectangle, null);
+        mColorBg = ResourcesCompat.getColor(getResources(), R.color.colorBackground, null);
+
+        mPaint.setColor(mColorBg);
+        mPaintText.setColor(ResourcesCompat.getColor(getResources(), R.color.colorText, null));
+        mPaintText.setTextSize(70);
+        mRect = new Rect();
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int vWidth = view.getWidth();
+                int vHeight = view.getHeight();
+                int halfWidth = vWidth/2;
+                int halfHeight = vHeight/2;
+
+                if (mOffset==OFFSET) {
+                    mBitmap = Bitmap.createBitmap(vWidth, vHeight, Bitmap.Config.ARGB_8888);
+                    mImageView.setImageBitmap(mBitmap);
+
+                    mCanvas = new Canvas(mBitmap);
+                    mCanvas.drawColor(mColorBg);
+                    mCanvas.drawText("Keep Tapping", 100, 100, mPaintText);
+                    mOffset += OFFSET;
+                }
+                else {
+                    if (mOffset < halfWidth && mOffset < halfHeight) {
+                        mPaint.setColor(mColorRect - MULTIPLIER*mOffset);
+
+                        mRect.set(mOffset, mOffset, vWidth - mOffset, vHeight - mOffset);
+                        mCanvas.drawRect(mRect, mPaint);
+                        mOffset += OFFSET;
+                    }
+                    else {
+                        int mColorAccent = ResourcesCompat.getColor(getResources(), R.color.orange, null);
+                        mPaint.setColor(mColorAccent);
+                        mCanvas.drawCircle(halfWidth, halfHeight, halfWidth/3, mPaint);
+
+                        String text = "Done";
+                        Rect mBounds = new Rect();
+                        mPaintText.getTextBounds(text, 0, text.length(), mBounds);
+                        int x = halfWidth - mBounds.centerX();
+                        int y = halfHeight - mBounds.centerY();
+                        mCanvas.drawText(text, x, y, mPaintText);
+                    }
+                }
+
+                view.invalidate();
+
+            }
+        });
+    }
+}
